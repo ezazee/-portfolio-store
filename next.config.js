27675d5cbdbed2e39de937d4b0511fd59fa4e1d1
ezazee <yukiirima123@gmail.com>
2022-08-23 10:43:53 +0700

    asdasd

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  assetPrefix: isProd ? "/store_nextJS/" : "",
+};
+
+module.exports = nextConfig;
